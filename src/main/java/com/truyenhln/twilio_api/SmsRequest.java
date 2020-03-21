61709f60d6b7e6deb393a39402a36afc546612af
truyenho95 <truyenho.webdev@gmail.com>
2020-03-20 22:19:17 -0500

    create twilio sms sender

---
+++ src/main/java/com/truyenhln/twilio_api/SmsRequest.java
@@ -0,0 +1,28 @@
+package com.truyenhln.twilio_api;
+
+public class SmsRequest {
+
+    private final String phoneNumber; // Destination phone number
+    private final String message;
+
+    public SmsRequest(String phoneNumber, String message) {
+        this.phoneNumber = phoneNumber;
+        this.message = message;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "SmsRequest{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
